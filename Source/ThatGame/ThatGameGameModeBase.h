58b6b336fb1319aa4a262ee379eb4ebce4a74ae2
Ivan Tsoi <98018454+DesPises@users.noreply.github.com>
2023-07-13 17:55:43 +0300

    Initial commit
    Project files generated

---
+++ Source/ThatGame/ThatGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ThatGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THATGAME_API AThatGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
