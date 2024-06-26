38c1a5e993b26faceedb270b723f1ea8649030ef
Ivan Tsoi <vanek-tsoy@mail.ru>
2024-06-26 22:03:43 +0300

    BP to C++
    - Almost all variables from BP_Boris converted to C++

--- Source/MyFall/Public/CPPBoris.h
+++ Source/MyFall/Public/CPPBoris.h
@@ -23,6 +23,81 @@ public:
 	UFUNCTION(BlueprintCallable)
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "States")
+	// Variables
+
+	// States
+	UPROPERTY(BlueprintReadWrite, Category = "States")
 	bool IsSliding;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool IsHiding;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool IsClimbing;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool IsRopeRiding;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool IsSettingTrap;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool IsRagdoll;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool IsGrabbing;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool IsJumping;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool HandsTied;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool SpeedBoostActive;
+
+	UPROPERTY(BlueprintReadWrite, Category = "States")
+	bool IsInvulnerable;
+
+	// Can do
+	UPROPERTY(BlueprintReadWrite, Category = "Can do")
+	bool CanMove;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Can do")
+	bool CanGrab;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Can do")
+	bool CanInteract;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Can do")
+	bool CanUnhide;
+
+	// Parameters
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	float Sensitivity = 0.5f;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	float Pitch;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	float GrabSocketZDefault;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	float YawOutput;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	float HandsPositionWhileWalking;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	float DefaultJumpMaxZ;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	float DefaultWalkSpeed;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	bool GrabbedObjectIsClipping;
+
+	UPROPERTY(BlueprintReadWrite, Category = "Parameters")
+	int HP = 3;
+
 };
